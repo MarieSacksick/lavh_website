5a6b9703a49a7f9f1ffdc79f3a3a2191980f9b4e
Clémence Fernandez <clemencefrz@gmail.com>
2025-05-15 16:14:00 +0200

    Update ActorsPictures component

--- app/components/ActorsPictures/ActorsPictures.tsx
+++ app/components/ActorsPictures/ActorsPictures.tsx
@@ -3,11 +3,21 @@ import Image, { ImageProps } from 'next/image';
 
 type ActorPictureProps = Pick<ImageProps, 'alt' | 'src'>;
 
+const actors = [
+  { name: 'Thomas Sacksick', image: '/actors/Sacksick_Thomas.jpg' },
+  { name: 'Mélodie Richard', image: '/actors/Richard_melodie_24.jpg' },
+  { name: 'Marion Rochmann', image: '/actors/Marion_Rochmann_20230827 copie.jpg' },
+  { name: 'André Marcon', image: '/actors/Marcon_Andre_24.jpg' },
+  { name: 'Guillaume Lévèque', image: '/actors/Lévèque_Guillaume_24.jpg' },
+  { name: 'Evelyne Istria', image: '/actors/Istria_Evelyne_24.jpg' },
+  { name: 'Louis Albertosi', image: '/actors/Albertosi_Louis.jpg' },
+];
+
 function ActorPicture({ alt, src }: ActorPictureProps) {
   return (
     <div className={styles.polaroidContainer}>
       <Image src={src} alt={alt} width={300} height={300} className={styles.actorImage} />
-      <div className={styles.name}>Thomas Sacksick</div>
+      <div className={styles.name}>{alt}</div>
     </div>
   );
 }
@@ -15,13 +25,9 @@ function ActorPicture({ alt, src }: ActorPictureProps) {
 export default function ActorsPictures() {
   return (
     <div className={styles.container}>
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
-      <ActorPicture alt="Thomas Sacksick" src={'/actors/Sacksick_Thomas.jpg'} />
+      {actors.map(actor => (
+        <ActorPicture key={actor.name} alt={actor.name} src={actor.image} />
+      ))}
     </div>
   );
 }
