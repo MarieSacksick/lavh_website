990bc5ed1fc06e91415d1ab6776301deb37d45a6
Clémence Fernandez <clemencefrz@gmail.com>
2025-05-16 16:25:38 +0200

    Update footer

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -9,48 +9,59 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className={styles.container}>
-      <h2>Contactez-nous</h2>
-      <div className={styles.content}>
-        <div className={styles.iconsContainer}>
-          <a
-            href="https://www.facebook.com/litteratureavoixhaute/?locale=fr_FR"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-          >
-            <FaFacebook className={styles.icon} />
-          </a>
-          <a
-            href="https://www.youtube.com/@lavh8723"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.iconLink}
-          >
-            <FaYoutube className={styles.icon} />
-          </a>
-          <a href="mailto:contact@litteratureavoixhaute.com" className={styles.iconLink}>
-            <IoMail className={styles.icon} />
-          </a>
-          <a href="tel:0231228381" className={styles.iconLink}>
-            <BsTelephoneFill className={styles.icon} />
-          </a>
+    <footer className={styles.footer}>
+      <Image
+        src={'divider_ornament.png'}
+        alt=""
+        width={100}
+        height={100}
+        className={styles.dividerOrnament}
+      />
+      <div className={styles.footerContent}>
+        <div className={styles.content}>
+          <div className={styles.iconsAndTitleContainer}>
+            <h2>Contactez-nous</h2>
+            <div className={styles.iconsContainer}>
+              <a
+                href="https://www.facebook.com/litteratureavoixhaute/?locale=fr_FR"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.iconLink}
+              >
+                <FaFacebook className={styles.icon} />
+              </a>
+              <a
+                href="https://www.youtube.com/@lavh8723"
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.iconLink}
+              >
+                <FaYoutube className={styles.icon} />
+              </a>
+              <a href="mailto:contact@litteratureavoixhaute.com" className={styles.iconLink}>
+                <IoMail className={styles.icon} />
+              </a>
+              <a href="tel:0231228381" className={styles.iconLink}>
+                <BsTelephoneFill className={styles.icon} />
+              </a>
+            </div>
+          </div>
+          <Image
+            src={'head_from_logo.png'}
+            alt=""
+            width={100}
+            height={100}
+            className={styles.headImage}
+          />
         </div>
-        <Image
-          src={'head_from_logo.png'}
-          alt=""
-          width={100}
-          height={100}
-          className={styles.headImage}
-        />
-      </div>
-      <div className={styles.bottom}>
-        <p className={styles.copyright}>
-          © {currentYear} Littérature à Voix Haute. Tous droits réservés.
-        </p>
-        <Link href="/cgu" className={styles.legalLink}>
-          Conditions Générales d&apos;Utilisation
-        </Link>
+        <div className={styles.bottom}>
+          <p className={styles.copyright}>
+            © {currentYear} Littérature à Voix Haute. Tous droits réservés.
+          </p>
+          <Link href="/cgu" className={styles.legalLink}>
+            Conditions Générales d&apos;Utilisation
+          </Link>
+        </div>
       </div>
     </footer>
   );
