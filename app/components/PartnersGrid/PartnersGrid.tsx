39d4c7c6f1689db5cdfc752921f51ace93ab78bf
Clémence Fernandez <clemencefrz@gmail.com>
2025-05-23 08:53:32 +0200

    the partners are links and redirect to the partner website

--- app/components/PartnersGrid/PartnersGrid.tsx
+++ app/components/PartnersGrid/PartnersGrid.tsx
@@ -1,30 +1,39 @@
-import Image from 'next/image';
 import styles from './PartnersGrid.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const partners = [
+type Partner = { name: string; logo: string; website: string };
+
+const partners: Partner[] = [
   {
     name: 'Conseil Général du Calvados',
     logo: '/logos/conseil_général_calvados.jpeg',
+    website: 'https://www.calvados.fr/accueil.html',
   },
   {
     name: 'Creully sur Seulles',
     logo: '/logos/logo Creully sur Seulles copie.png',
+    website: 'https://creully-sur-seulles.fr/',
   },
   {
     name: 'Tracy sur Mer',
     logo: '/logos/Logo Tracy copie.jpg',
+    website: 'https://tracy-sur-mer.fr/',
   },
   {
     name: 'Seulles Terre et Mer',
     logo: '/logos/Logo_Seulles_terre et mer copie.jpg',
+    website: 'https://seulles-terre-mer.fr/',
   },
   {
     name: 'Friendly Home Bayeux',
     logo: '/logos/Friendly Home Bayeux.PNG',
+    website: 'https://www.facebook.com/friendlyhomebayeux/',
   },
   {
-    name: 'Image 003',
-    logo: '/logos/image003 copie.jpg',
+    name: 'Arromanches Loisirs Culture',
+    logo: '/logos/Arromanches Loisirs Culture.jpg',
+    website: 'https://www.arromanches.com/associations/details.php?assoc_id=32',
   },
 ];
 
@@ -34,7 +43,13 @@ export default function PartnersGrid() {
       <h2>Nos partenaires</h2>
       <div className={styles.logosContainer}>
         {partners.map(partner => (
-          <div key={partner.name} className={styles.logoWrapper}>
+          <Link
+            key={partner.name}
+            href={partner.website}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.logoWrapper}
+          >
             <Image
               src={partner.logo}
               alt={`Logo ${partner.name}`}
@@ -42,7 +57,7 @@ export default function PartnersGrid() {
               height={100}
               className={styles.logo}
             />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
