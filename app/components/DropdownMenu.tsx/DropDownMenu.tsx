f5e8fdb2781bce95cda005c1d2fe029906fdbe3b
Clémence Fernandez <clemencefrz@gmail.com>
2025-05-23 10:03:56 +0200

    Style Dropdown

--- app/components/DropdownMenu.tsx/DropDownMenu.tsx
+++ app/components/DropdownMenu.tsx/DropDownMenu.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { ArrowDownIcon } from '@heroicons/react/24/solid';
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import styles from './DropDownMenu.module.css';
 
 type Content = {
@@ -16,7 +15,7 @@ const DropDownMenu = ({ triggerTitle, contents }: DropDownMenuProps) => {
   return (
     <div className={styles.dropdown}>
       <button className={styles.trigger}>
-        {triggerTitle} <ArrowDownIcon className={styles.icon} />
+        {triggerTitle} <ChevronDownIcon className={styles.icon} />
       </button>
       <div className={styles.content}>
         {contents.map((content, key) => (
